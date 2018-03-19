8a99589bf6b1d33403308e898b08ba2a7904744d
Dmitry Novik <dmitry.novik@gmail.com>
2018-03-19 11:58:38 +1100

    Correcting the refactoring

--- WorkflowCoreSample/MainWorkflow.cs
+++ WorkflowCoreSample/MainWorkflow.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Threading;
 using WorkflowCore.Interface;
-using WorkflowCore.Primitives;
 
 namespace WorkflowCoreSample
 {
@@ -14,50 +12,6 @@ namespace WorkflowCoreSample
             protected override void RunImpl(IStepExecutionContext context)
             {
                 Console.WriteLine(Text);
-            }
-        }
-
-        public class EnterName : StepBase
-        {
-            public string Name { get; set; }
-
-            protected override void RunImpl(IStepExecutionContext context)
-            {
-                Console.WriteLine("\nPlease enter your name or hit ENTER to exit");
-                Name = Console.ReadLine();
-            }
-        }
-
-        public class TakePhoto : StepBase
-        {
-            static readonly Random Rand = new Random(DateTime.Now.GetHashCode());
-
-            public string Name { get; set; }
-
-            protected override void RunImpl(IStepExecutionContext context)
-            {
-                Console.WriteLine($"{Name}, let us take your photo.");
-
-                // pretend we are doing something ...
-                Thread.Sleep(1000);
-
-                if (Rand.NextDouble() > 0.5)
-                {
-                    // simulate error with probability of 50%
-                    const string error = "ERR: Camera failure";
-                    Console.WriteLine(error);
-                    throw new Exception(error);
-                }
-            }
-        }
-
-        public class Finish : StepBase
-        {
-            public string Message { get; set; }
-
-            protected override void RunImpl(IStepExecutionContext context)
-            {
-                Console.WriteLine(Message);
             }
         }
 
--- WorkflowCoreSample/PersonWorkflow.cs
+++ WorkflowCoreSample/PersonWorkflow.cs
@@ -1,26 +1,72 @@
-﻿using WorkflowCore.Interface;
+﻿using System;
+using System.Threading;
+using WorkflowCore.Interface;
 
 namespace WorkflowCoreSample
 {
     public class PersonWorkflow : IChildWorkflow<DataContext>
     {
+        public class EnterName : StepBase
+        {
+            public string Name { get; set; }
+
+            protected override void RunImpl(IStepExecutionContext context)
+            {
+                Console.WriteLine("\nPlease enter your name or hit ENTER to exit");
+                Name = Console.ReadLine();
+            }
+        }
+
+        public class TakePhoto : StepBase
+        {
+            static readonly Random Rand = new Random(DateTime.Now.GetHashCode());
+
+            public string Name { get; set; }
+
+            protected override void RunImpl(IStepExecutionContext context)
+            {
+                Console.WriteLine($"{Name}, let us take your photo.");
+
+                // pretend we are doing something ...
+                Thread.Sleep(1000);
+
+                if (Rand.NextDouble() > 0.5)
+                {
+                    // simulate error with probability of 50%
+                    const string error = "ERR: Camera failure";
+                    Console.WriteLine(error);
+                    throw new Exception(error);
+                }
+            }
+        }
+
+        public class Finish : StepBase
+        {
+            public string Message { get; set; }
+
+            protected override void RunImpl(IStepExecutionContext context)
+            {
+                Console.WriteLine(Message);
+            }
+        }
+
         public void Build(IWorkflowBuilder<DataContext> workflow)
         {
             workflow
                 // Enter person name:
-                .StartWith<MainWorkflow.EnterName>()
+                .StartWith<EnterName>()
                 .Output(data => data.Name, step => step.Name)
                 .If(data => !string.IsNullOrEmpty(data.Name))
                 .Do(y =>
                     // Take photo:
-                        y.StartWith<MainWorkflow.TakePhoto>()
+                        y.StartWith<TakePhoto>()
                             .Input(step => step.Name, data => data.Name)
                             .Output(data => data.Name, step => step.Name)
                             // handle camera errors => retry
                             // what it lacks here is the different handler / policy for different errors
                             //.OnError(WorkflowErrorHandling.Retry, TimeSpan.FromSeconds(2))
                             // Say 'thank you' and loop to the next person:
-                            .Then<MainWorkflow.Finish>()
+                            .Then<Finish>()
                             .Input(step => step.Message, data => $"Thank you {data.Name}, you are all done.")
                 );
         }
